fa969a3b708bf84af767e0efac016cfcb32696c6
Anshu Kumar <ak5785598@gmail.com>
2025-09-11 23:51:55 +0000

    first you have to review about wafa miss you and haza  inter face everyt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&family=Belleza&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Lexend:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </head>
